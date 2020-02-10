87b48ee3b5bd9b5d6968151278ee6c294a6799fb
Ryan O'Toole <ryanotoole@Ryans-MacBook-Air.local>
2020-02-10 13:58:22 -0600

    finsihed spin words need to solve 4 more tests tomorrow

--- algorithm.js
+++ algorithm.js
@@ -24,3 +24,120 @@ function reverseWords(string) {
   const string = 'a dog is a god';
   reverseWords(string);
   
+
+
+
+  function reverseWords(string) {
+    const stringArr = string.split('');
+    let counter = 0;
+    let regWord = '';
+    let revWord = '';
+    let finalArr = [];
+    console.log(finalArr);
+    for (let letter of stringArr) {
+      if (letter === ' ') {
+        if (counter >= 5) {
+          revWord += letter
+          finalArr.push(revWord);
+          console.log(revWord);
+          revWord = '';
+          regWord = '';
+          counter === -1;
+        }
+      }
+      else {
+        finalArr.push(regWord)
+        regWord = '';
+        revWord = '';
+      }
+      if (finalArr.join('').length + regWord === string.length - 1) {
+        finalArr.push(regWord);
+      }
+      if (finalArr.join('').length + revWord === string.length - 1) {
+        finalArr.push(revWord);
+      }
+      regWord += letter;
+      revWord = letter + revWord;
+      counter++;
+    }
+    console.log('final', finalArr.join(' '));
+    return finalArr.join(' ');
+  }
+  const string = 'a dog named Roverdog ran for President';
+  reverseWords(string);
+
+  
+
+
+function reverseWords(string) {
+    const stringArr = string.split('');
+    let counter = 0;
+    let regWord = '';
+    let revWord = '';
+    let finalArr = [];
+    for (let letter of stringArr) {
+      if (letter === ' ') {
+        if (counter >= 5) {
+          revWord += letter
+          finalArr.push(revWord);
+          revWord = '';
+          regWord = '';
+          counter = -1;
+        }
+        else {
+          regWord += letter;
+          finalArr.push(regWord);
+          regWord = '';
+          revWord = '';
+          counter = -1;
+        }
+      }
+   
+    if (finalArr.join('').length + revWord.length === string.length) {
+      if (regWord.length <= 5) {
+         finalArr.push(regWord);
+      }
+      else {
+        finalArr.push(revWord)
+      }
+    }
+      regWord += letter;
+      revWord = letter + revWord;
+      counter++;
+    }
+    return finalArr.join(' ');
+  }
+  const string = 'a dog named Roverdog ran for President things';
+  reverseWords(string);
+  
+
+
+function spinWords(string) {
+    const stringArr = string.split('');
+    let regWord = '';
+    let revWord = '';
+    let finalArr = [];
+    for (let letter of stringArr) {
+      if (letter === ' ') {
+        if (revWord.length < 5) {
+          finalArr.push(regWord);
+        }
+        else {
+          finalArr.push(revWord);
+        }
+        revWord = '';
+        regWord = '';
+    } else {
+        regWord += letter;
+        revWord = letter + revWord;
+      }
+      if (finalArr.join(' ').length + revWord.length === string.length - 1) {
+        if (revWord.length < 5) {
+          finalArr.push(regWord);
+        } else {
+          finalArr.push(revWord);
+        }
+      }
+    }
+    return finalArr.join(' ');
+  }
